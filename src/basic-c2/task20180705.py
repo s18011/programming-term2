33fd6862316d10cea005ef37763514ca4ec18b38
email <s18011@std.it-college.ac.jp>
2018-07-10 16:56:40 +0900

    課題修正

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -1,14 +1,17 @@
-print((200*2)+(100)+((100*0.8)*2)-150)
-beal_v = 200
+#print((200*2)+(100)+((100*0.8)*2)-150)
+beer_v = 200
 otumami_v = 100
 yakitori_v = 100
 
-beal_c = 2
+beer_c = 2
 otumami_c = 1
 yakitori_c = 2
 
-rate = 0.8
+rate = 0.2
 
-sum_v = (beal_v * beal_c) + (otumami_v * otumami_c) + (yakitori_v * yakitori_c)
-payment = sum_v * rate
+point_nebiki = -150
+sum_v = (beer_v * beer_c) + (otumami_v * otumami_c) + (yakitori_v * yakitori_c * (1-rate))
+payment = sum_v + point_nebiki
 
+print("買い物の合計は", sum_v, "円")
+print("割引してもらうと", payment, "円")
