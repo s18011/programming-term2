68cc3546fd816154a0306011741f9a8b42651542
email <s18011@std.it-college.ac.jp>
2018-08-28 16:48:34 +0900

    課題

---
+++ src/algo-p5/0828/q27/field_map.py
@@ -0,0 +1,74 @@
+import random
+
+# 変数定義部
+goal_pos = 100  # ゴールの位置
+
+# 関数定義部
+def shake_dice():
+    """
+    サイコロを振る
+
+    Parameters
+    ----------
+    なし
+
+    Returns
+    -------
+    int
+        サイコロを振って出た目
+    """
+    # サイコロを振るため、Enterキーを入力待ちにする
+    input("サイコロを振ります。Enterキーを押してください。")
+
+    # 1〜6の任意の数字を取得する
+    dice_num = random.randint(1, 6)
+
+    # 出た目の数をコンソールに表示する
+    print(str(dice_num) + "の目が出ました。")
+
+    # 出た目を返す
+    return dice_num
+
+def get_event(target_cell):
+    """
+    止まったセルのイベントを取得する
+
+    Parameters
+    ----------
+    target_cell : int
+        止まったセル
+
+    Returns
+    -------
+    str
+        イベントの名称
+    """
+    global goal_pos
+
+    if target_cell < goal_pos:
+        if target_cell % 5 == 0:
+            return "BattleVsZako"
+        elif target_cell % 10 == 3:
+            return "GoMoreForward"
+        elif target_cell % 20 == 17:
+            return "GoBack"
+        elif target_cell % 30 == 26:
+            # :target_cellを30で割った余りが26の場合に、"HealingLake"を返してください。
+            return "HealingLake"
+        elif target_cell % 30 == 8:
+            # :target_cellを30で割った余りが8の場合に、"PoisonSwamp"を返してください。
+            return "PoisonSwamp"
+        elif target_cell % 100 == 49:
+            return "GoBackToStart"
+        else:
+            return ""
+
+# 以下メイン処理
+if __name__ == '__main__':
+    print(get_event(1))
+
+    print(get_event(13))
+
+    print(get_event(37))
+
+    print(get_event(49))
---
+++ src/algo-p5/0828/q27/player.py
@@ -0,0 +1,400 @@
+import field_map
+import sys
+import random
+from enemy import Enemy
+
+class Player:
+    def __init__(self, name):
+        """
+        コンストラクタ
+
+        Parameters
+        ----------
+        name : str
+            プレイヤーの名前
+
+        Returns
+        -------
+        自分自身のインスタンス
+        """
+        self.name = name
+        self.cur_pos = 0
+        self.hp = 100
+        self.max_hp = 100
+        self.min_damage = 4
+        self.max_damage = 7
+        self.freq = 10
+        self.plant_nums = 10
+        self.exp = 0
+        self.level = 1
+
+    def choose_action_in_field(self):
+        """
+        フィールド中での操作を選択する
+
+        Parameters
+        ----------
+        なし
+
+        Returns
+        -------
+        なし
+        """
+        # 見やすさのために、空白行を表示
+        print()
+
+        # 「何をしますか?」を表示
+        print("何をしますか?")
+
+        # 「1:サイコロを振る、2:現在の状態を確認する、3:薬草を使う、9:ゲームを終了する>> 」を表示し、入力待ちにする
+        cmd_num = input("1:サイコロを振る、2:現在の状態を確認する、3:薬草を使う、9:ゲームを終了する>> ")
+
+        # cmd_numの値によって条件分岐
+        if cmd_num == "1":
+            # その場から動く
+            self.move()
+        elif cmd_num == "2":
+            # 状態を表示する
+            self.show_status()
+        elif cmd_num == "3":
+            # 薬草を使う
+            self.use_plants()
+        elif cmd_num == "9":
+            # ゲームを終了する
+            self.quit_game()
+
+    def move(self):
+        """
+        動く(サイコロを振る行為を含む)
+
+        Parameters
+        ----------
+        なし
+
+        Returns
+        -------
+        なし
+        """
+        # サイコロを振る
+        dice_num = field_map.shake_dice()
+
+        # 出た目の数だけ前に進む
+        self.go_forward(dice_num)
+
+    def go_forward(self, cells):
+        """
+        前に進む
+
+        Parameters
+        ----------
+        cells : int
+            進むマス目の数
+
+        Returns
+        -------
+        なし
+        """
+        # 引数のマス目だけ進む
+        self.cur_pos += cells
+
+        # 現在位置を表示
+        print("現在位置は" + str(self.cur_pos) + "です。")
+
+        # 止まったマス目のイベントを取得する
+        event_nm = field_map.get_event(self.cur_pos)
+
+        if event_nm == "BattleVsZako":
+            # ザコキャラ「スラスラ」と戦う
+            zako = Enemy("スラスラ")
+            self.battle(zako)
+        elif event_nm == "GoMoreForward":
+            # 2マスさらに前に進む
+            self.go_more_forward(2)
+        elif event_nm == "GoBack":
+            # 3マス戻る
+            self.go_back(3)
+        elif event_nm == "GoBackToStart":
+            # 振り出しに戻る
+            self.go_back_to_start()
+        elif event_nm == "HealingLake":
+            self.healed_in_lake()
+            # :event_nmが"HealingLake"の場合、新たに定義したself.healed_in_lake()を呼び出してください。
+        elif event_nm == "PoisonSwamp":
+            self.poisoned_in_swamp()
+            # :event_nmが"PoisonSwamp"の場合、新たに定義したself.poisoned_in_swamp()を呼び出してください。
+
+    def go_more_forward(self, cells):
+        """
+        出た目の分さらに前に進む
+
+        Parameters
+        ----------
+        cells : int
+            進むマス目の数
+
+        Returns
+        -------
+        なし
+        """
+        print("イベント発生！" + str(cells) + "マスさらに進みます。")
+
+        # 引数で渡された目の分だけ前に進む
+        self.go_forward(cells)
+
+    def go_back(self, cells):
+        """
+        出た目の分後ろに戻る
+
+        Parameters
+        ----------
+        cells : int
+            戻るマス目の数
+
+        Returns
+        -------
+        なし
+        """
+        print("イベント発生！" + str(cells) + "マス後ろに戻ります。")
+
+        # 引数で出た目の分だけ前に戻る(引数に-1を掛けることで戻る動作をしている)
+        self.go_forward((cells * -1))
+
+    def go_back_to_start(self):
+        """
+        出た目の分後ろに戻る
+
+        Parameters
+        ----------
+        なし
+
+        Returns
+        -------
+        なし
+        """
+        print("イベント発生！振り出しに戻ってしまいます！")
+
+        # 引数で出た目の分だけ前に戻る(引数に-1を掛けることで戻る動作をしている)
+        self.go_forward((self.cur_pos * -1))
+
+    def show_status(self):
+        """
+        現在の状態を表示する
+
+        Parameters
+        ----------
+        なし
+
+        Returns
+        -------
+        なし
+        """
+        # 状態を表示する
+        print(self.name + "の現在位置は" + str(self.cur_pos) + "、HPは" + str(self.hp) + "です。")
+        # 薬草の枚数も表示する。
+        print("薬草を" + str(self.plant_nums) + "枚持っています。")
+
+    def battle(self, enemy):
+        """
+        敵とたたかう
+
+        Parameters
+        ----------
+        enemy : Enemy
+            敵のオブジェクト
+
+        Returns
+        -------
+        なし
+        """
+        # イベント発生メッセージ
+        print("イベント発生！" + enemy.name + "があらわれた！")
+
+        # 敵が倒されるまで戦い続ける
+        while enemy.hp > 0:
+            # 見やすさのために空行を表示
+            print()
+
+            # ガイドメッセージを表示
+            print("どうする?")
+
+            # 「1:攻撃する、3:薬草を使う、9:逃げる>> 」を表示し、入力待ちにする
+            cmd_num = input("1:攻撃する、3:薬草を使う、9:逃げる>> ")
+
+            if cmd_num == "1":
+                # プレイヤーが敵を攻撃。倒したらループを抜ける
+                if self.attack(enemy):
+                    break
+            elif cmd_num == "3":
+                # 薬草を使う
+                self.use_plants()
+            elif cmd_num == "9":
+                # 逃げる
+                print(self.name + "は逃げ出した!")
+                return
+
+            # 敵がプレイヤーを攻撃。倒されたらゲームオーバー
+            if not enemy.attack(self):
+                print(self.name + "はしんでしまった!世界は闇に包まれてしまった...")
+                sys.exit()
+
+        # バトル終了
+        print(self.name + "は" + enemy.name + "を倒した!")
+
+    def attack(self, enemy):
+        """
+        敵を攻撃する
+
+        Parameters
+        ----------
+        enemy : Enemy
+            敵のオブジェクト
+
+        Returns
+        -------
+        bool
+            True:敵を倒した、False:敵がまだ生きている
+        """
+        # ダメージを最小〜最大の範囲でランダムに取得
+        damage = random.randint(self.min_damage, self.max_damage)
+
+        is_critical = False # 「かいしんのいちげき」かどうか
+        # 1/(self.freq)の確率で「かいしんのいちげき」を出す
+        rand_num = random.randint(1, self.freq)
+        if rand_num % self.freq == 0:
+            is_critical = True
+
+        # 自分のターンのメッセージ表示
+        print(self.name + "のこうげき!")
+
+        # かいしんのいちげきの場合、ダメージを倍にする
+        if is_critical:
+            print("かいしんのいちげき!!")
+            damage *= 2
+
+        # 相手にダメージを与える
+        enemy.hp -= damage
+
+        if enemy.hp > 0:
+            print(enemy.name + "に" + str(damage) + "のダメージを与えた!" + enemy.name + "のHPは" + str(enemy.hp) + "です。")
+            return False
+        else:
+            print(enemy.name + "に" + str(damage) + "のダメージを与えた!" + enemy.name + "のHPは0です。")
+            return True
+
+    def use_plants(self):
+        """
+        薬草を使う
+
+        Parameters
+        ----------
+        なし
+
+        Returns
+        -------
+        なし
+        """
+        # 薬草を持っていなければ、その旨表示して終了
+        if self.plant_nums <= 0:
+            print(self.name + "は薬草を持っていない")
+            return
+
+        # メッセージ表示
+        print(self.name + "は薬草を使った!")
+
+        # HPを30ポイント回復
+        self.hp += 30
+
+        # HPが最大を超えないように調整
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+
+        # 持っている薬草を1枚減らす
+        self.plant_nums -= 1
+
+        # 回復したHPの状態を表示
+        print(self.name + "のHPが" + str(self.hp) + "に回復した!")
+
+    # :healed_in_lakeメソッドを定義します。引数はselfのみです。
+    def healed_in_lake(self):
+        """
+        湖でHPを回復される
+
+        Parameters
+        ----------
+        なし
+
+        Returns
+        -------
+        なし
+        """
+        # :「イベント発生！癒しの湖で身を清めます。」を表示してください。
+        print('イベント発生！癒やしの湖で身を清めます。')
+
+        # :HPを最大まで回復します。self.hpにself.max_hpを代入してください。
+        self.hp = self.max_hp
+
+        # :「(self.name)のHPが全回復した!現在のHPは(self.hp)です。」を表示してください。
+        print(self.name, 'のHPが全回復した！現在のHPは', self.hp, 'です。')
+
+    # :poisoned_in_swampメソッドを定義します。引数はselfのみです。
+    def poisoned_in_swamp(self):
+        """
+        沼で毒に冒される
+
+        Parameters
+        ----------
+        なし
+
+        Returns
+        -------
+        なし
+        """
+        # :「イベント発生！沼で毒に冒されました。」を表示してください。
+        print('イベント発生!沼で毒に冒されました。')
+
+        # :20のダメージを受けます。self.hpから20を引いて(self.hpに再代入して)ください。
+        self.hp - 20
+        self.hp = 20
+
+        if self.hp > 0:
+            print(self.name, 'は20のダメージを受けた!現在のHPは', self.hp, 'です。')
+            # :self.hpが0より大きい場合、「(self.name)は20のダメージを受けた!現在のHPは(self.hp)です。」を表示してください。
+
+        else:
+            # :上記以外の場合、「(self.name)は20のダメージを受けた!(self.name)はしんでしまった!世界は闇に包まれてしまった...」を表示してください。
+            print(self.name, 'は20のダメージを受けた！', self.name, 'は死んでしまった!世界は闇に包まれてしまった...')
+            # :ゲームオーバーなので終了です。1つ前のメッセージに続けて、sys.exit()を呼び出してください。
+            sys.exit()
+
+    def quit_game(self):
+        """
+        ゲームを終了する
+
+        Parameters
+        ----------
+        なし
+
+        Returns
+        -------
+        なし
+        """
+        # 終了するかどうかの確認メッセージを表示
+        cmd_str = input("ゲームの状態はセーブされません。終了しますか?(y/n) ")
+
+        # Yが押されたら終了
+        if cmd_str.upper() == "Y":
+            sys.exit()
+
+# 以下メイン処理
+if __name__ == '__main__':
+    # プレイヤーのオブジェクトを作成
+    kevin = Player("ケビン")
+
+    # 敵のオブジェクトを作成
+    enemy = Enemy("スラスラ")
+
+    # ケビンとスラスラが戦う
+    kevin.battle(enemy)
+
+    # バトル後のケビンのステータスを表示
+    kevin.show_status()
